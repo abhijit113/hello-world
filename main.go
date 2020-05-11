e2fd8af12ecc79082da5ce97ec76d67f69d6954f
abhijit113 <abhijitsen113@gmail.com>
2020-05-12 02:06:00 +0530

    new commit

--- main.go
+++ main.go
@@ -3,6 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	 fmt.Println("hi")
+	fmt.Println("hi....I here....doing control check")
+	foo()
+	fmt.Println("This will print after foo")
 
+	for i := 0; i < 10; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+
+	}
+}
+
+func foo() {
+	fmt.Println("I am at function foo")
 }
